0babac8183eed408073cd22f60d36cdf51c9c2d4
yassine <yassineaghla23@gmail.com>
2026-03-25 15:43:49 +0100

    ajouter la query pour calculer le nombre des assist d'un joueur

--- src/main/java/org/example/ftblr/Repository/MatchGoalRepository.java
+++ src/main/java/org/example/ftblr/Repository/MatchGoalRepository.java
@@ -2,6 +2,8 @@ package org.example.ftblr.Repository;
 
 import org.example.ftblr.Entity.MatchGoal;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,5 +12,7 @@ import java.util.UUID;
 @Repository
 public interface MatchGoalRepository extends JpaRepository<MatchGoal, UUID> {
     List<MatchGoal> findByMatchId(UUID matchId);
-    long countByScorerId(UUID scorerId);
+    int countByScorerId(UUID scorerId);
+    @Query("SELECT COUNT(g) FROM MatchGoal g WHERE g.assistant.id = :assistId")
+    int countByAssistId(@Param("assistId") UUID assistId);
 }
